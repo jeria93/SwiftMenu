000efb2c4f24cf106c7408cb6d0f73d33290a822
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-05 23:13:46 +0100

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
