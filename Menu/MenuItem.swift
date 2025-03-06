a7b4765ae0a816aad642f16d94b3d1e70a5dd4e4
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-06 21:28:07 +0100

    Introduce MenuItem struct and dynamic MenuView
    Created a MenuItem struct with name, price, and imageName properties
    Defined a @State array of MenuItem in MenuView to handle and display menu items
    Replaced the default ContentView reference with MenuView, preparing the foundation for a sushi menu interface

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,26 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-03-06.
+//
+
+import Foundation
+
+struct MenuItem {
+    
+    var name: String
+    var price: String
+    var imageName: String
+    
+    func testPrint() { print(name) }
+    
+}
+
+//det som en cell kommer innehålla är en struct
+
+var menu1: MenuItem = MenuItem(name: "Burger", price: "10kr", imageName: "burger")
+var menu2: MenuItem = MenuItem(name: "Pommes Frites", price: "5kr", imageName: "pommesfrites")
+var menu3: MenuItem = MenuItem(name: "Coca Cola", price: "2kr", imageName: "coca-cola")
+
+
--- Menu/MenuView.swift
+++ Menu/MenuView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct MenuView: View {
+    
+    @State private var menuItems: [MenuItem] = []
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
