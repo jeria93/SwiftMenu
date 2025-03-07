6079ab709bb36bdae92823f9f8b25f5cea7fde97
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-07 14:28:57 +0100

    Refactor menu data into a DataService and load on appear
    Introduced a new DataService struct returning an array of sushi MenuItems
    Removed the hardcoded menuItems in MenuView and replaced it with a data-driven approach
    Populated menuItems in onAppear using data.getData() for cleaner code separation and improved flexibility

--- Menu/MenuView.swift
+++ Menu/MenuView.swift
@@ -9,68 +9,8 @@ import SwiftUI
 
 struct MenuView: View {
     
-    private var menuItems: [MenuItem] = [
-        MenuItem(
-            name: "Onigiri",
-            price: "1.99",
-            imageName: "onigiri"
-        ),
-        MenuItem(
-            name: "Meguro Sushi",
-            price: "5.99",
-            imageName: "meguro-sushi"
-        ),
-        MenuItem(
-            name: "Tako Sushi",
-            price: "4.99",
-            imageName: "tako-sushi"
-        ),
-        MenuItem(
-            name: "Avocado Maki",
-            price: "2.99",
-            imageName: "avocado-maki"
-        ),
-        MenuItem(
-            name: "Tobiko Spicy Maki",
-            price: "4.99",
-            imageName: "tobiko-spicy-maki"
-        ),
-        MenuItem(
-            name: "Salmon Sushi",
-            price: "4.99",
-            imageName: "salmon-sushi"
-        ),
-        MenuItem(
-            name: "Hamachi Sushi",
-            price: "6.99",
-            imageName: "hamachi-sushi"
-        ),
-        MenuItem(
-            name: "Kani Sushi",
-            price: "3.99",
-            imageName: "kani-sushi"
-        ),
-        MenuItem(
-            name: "Tamago Sushi",
-            price: "3.99",
-            imageName: "tamago-sushi"
-        ),
-        MenuItem(
-            name: "California Roll",
-            price: "3.99",
-            imageName: "california-roll"
-        ),
-        MenuItem(
-            name: "Shrimp Sushi",
-            price: "3.99",
-            imageName: "shrimp-sushi"
-        ),
-        MenuItem(
-            name: "Ikura Sushi",
-            price: "5.99",
-            imageName: "ikura-sushi"
-        )
-    ]
+    @State private var menuItems: [MenuItem] = []
+    private var data = DataService()
     
     var body: some View {
         
@@ -98,6 +38,9 @@ struct MenuView: View {
                 )
         }
         .listStyle(.plain)
+        .onAppear() {
+            menuItems = data.getData()
+        }
     }
 }
 
